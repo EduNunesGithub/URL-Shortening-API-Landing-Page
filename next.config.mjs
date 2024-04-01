83a51f68130d53c0aa301860f10f71f4f5bacb48
Edu Nunes <edu.nunes.github@gmail.com>
2024-03-31 21:39:17 -0300

    feat(pwa): config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 import bundle from "@next/bundle-analyzer";
+import withSerwistInit from "@serwist/next";
 
 const analyzer = bundle({
   enabled: process.env.ANALYZE === "true",
 });
 
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default analyzer(nextConfig);
+export default analyzer(withSerwist(nextConfig));
---
+++ src/app/sw.ts
@@ -0,0 +1,15 @@
+import { defaultCache } from "@serwist/next/browser";
+import type { PrecacheEntry } from "@serwist/precaching";
+import { installSerwist } from "@serwist/sw";
+
+declare const self: ServiceWorkerGlobalScope & {
+  __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+};
+
+installSerwist({
+  clientsClaim: true,
+  navigationPreload: true,
+  precacheEntries: self.__SW_MANIFEST,
+  runtimeCaching: defaultCache,
+  skipWaiting: true,
+});
